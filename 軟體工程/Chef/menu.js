14f32df4bdbad2c2e83490e8508bb4f5eb723c3e
missy60722 <81450774+missy60722@users.noreply.github.com>
2022-12-30 17:25:44 +0800

    Delete 軟體工程 directory

--- 軟體工程/Chef/menu.js
+++
@@ -1,7 +0,0 @@
-function onClick(){
-    var checkbox = document.getElementById("menu");
-    if (checkbox.checked = true){
-      console.log(checkbox.checked);
-      document.getElementById("dish").style.display = 'none';
-    }
-  }
--- 軟體工程/Staff/staff.js
+++
@@ -1,6 +0,0 @@
-function checkDesk(){
-    var checkbox = document.getElementById('first');
-    if (checkbox.checked = true){
-      window.location.assign("deskStep1.html");
-    }
-  }
