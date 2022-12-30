ae81691e47edafb9f6b7a4f45c497589f6dcc80c
missy60722 <81450774+missy60722@users.noreply.github.com>
2022-12-30 17:27:03 +0800

    Add files via upload

---
+++ 軟體工程/Chef/menu.js
@@ -0,0 +1,7 @@
+function onClick(){
+    var checkbox = document.getElementById("menu");
+    if (checkbox.checked = true){
+      console.log(checkbox.checked);
+      document.getElementById("dish").style.display = 'none';
+    }
+  }
