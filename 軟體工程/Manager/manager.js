fa3b4cf7cf01bb8c9b94cb6434336cf30128504a
missy60722 <81450774+missy60722@users.noreply.github.com>
2022-12-30 21:11:39 +0800

    Delete 軟體工程 directory

--- 軟體工程/Manager/manager.js
+++
@@ -1,6 +0,0 @@
-function checkDesk(){
-    var checkbox = document.getElementById('first');
-    if (checkbox.checked = true){
-      window.location.assign("/Users/missy/大二上/軟體工程/軟體工程/Staff/deskStep1.html");
-    }
-  }
